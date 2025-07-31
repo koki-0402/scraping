215d723765713de464566cd7b9322c593576106e
koki-0402 <tamtamuaaabbc@gmail.com>
2025-07-31 22:27:44 +0900

    コードの追加

---
+++ yahoo_scraper.py
@@ -0,0 +1,17 @@
+﻿import requests
+import re
+
+# Yahoo! JAPANニューストップページ
+url = 'https://news.yahoo.co.jp'
+res = requests.get(url)
+html = res.text
+
+# ニュースの見出し部分を正規表現で抽出
+titles = re.findall(r'<p class="sc-1t7ra5j-7 casbUp">(.*?)</p>', html)
+
+# 抽出結果を出力
+print("Yahoo!ニュース 見出し一覧")
+print("=" * 30)
+for i, title in enumerate(titles, start=1):
+    print(f"{i}. {title}")
+
